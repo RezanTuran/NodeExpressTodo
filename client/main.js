9b2de8fb14ae075fa0258ad76487d48f06ff2c5b
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-08-21 01:00:00 +0200

    NodeExpress todo app

---
+++ client/main.js
@@ -0,0 +1,108 @@
+
+window.addEventListener('load', getTodos)
+
+async function makeRequest(url, method, formdata) {
+    const response = await fetch(url, {
+    method: method,
+    body: formdata,
+    headers: {"Content-Type": "application/json"}
+})
+console.log(response);   
+
+const todos = await response.json()
+console.log(todos);
+}
+
+async function createTodo(event) {
+event.preventDefault() // Hinda ladda om sidan
+}
+
+// ### Get All Todos ### //
+ async function getTodos() {
+    const result = await fetch('/api/todos')
+    const todos = await result.json()
+    console.log(todos);
+
+    let container = document.getElementById("container")
+
+        for (let i = 0; i < todos.length; i++) {
+            let title = (todos[i].title);
+            let description = (todos[i].description);
+            let date = (todos[i].date);
+            let id = (todos[i].id);
+
+            let box = document.createElement("div");
+
+            let titleTag = document.createElement("h3");
+            let descriptionTag = document.createElement("p");
+            let dateTag = document.createElement("p");
+            let deleteButton = document.createElement("button");
+            let updateButton = document.createElement("button");
+
+            titleTag.innerText = title;
+            descriptionTag.innerText = description;
+            dateTag.innerText = date;
+            deleteButton.innerText = "Delete";
+            updateButton.innerText = "Update";
+
+            deleteButton.style.background = "#B35462";
+            updateButton.style.background = "#B35462";
+
+            deleteButton.onclick = function () {
+                deleteTodos(id);
+            }
+            updateButton.onclick = function () {
+                updateTodos(id);
+            }
+
+            box.appendChild(titleTag);
+            box.appendChild(descriptionTag);
+            box.appendChild(dateTag);
+            box.appendChild(deleteButton);
+            box.appendChild(updateButton);
+
+            container.appendChild(box);
+}
+}
+
+// ### Post Todos ### //
+function addTodos() {
+    
+    let todosInput = document.getElementById("todosInput").value
+    let todosDescription = document.getElementById("todosDescription").value
+    let todosDate = document.getElementById("todosDate").value
+    
+    if(todosInput === "" || todosDate === ""){
+        alert("Fyll uppgifter som behövs")
+    }else{
+    let body = {
+        "title": todosInput,
+        "description": todosDescription,
+        "date": todosDate
+    }
+    makeRequest("/api/todos", "post", JSON.stringify(body))
+    location.reload()
+}
+}
+
+function deleteTodos(id) {
+
+    makeRequest('/api/todos/' + id, 'delete')
+    location.reload()
+}
+
+
+
+function updateTodos(id) {
+
+    let todo = {
+        title:"dsfdsfsdf",
+        description:"Dfgdfdf",
+        date:"2020-01-01"
+    }
+
+    makeRequest('/api/todos/' + id, 'put', JSON.stringify(todo))
+    
+}
+
+
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+
+// Define a state for our data (database)
+let idIndex = 1
+
+const todos = [{
+    id: 0,
+    title: "Plugga",
+    description: "plugga efter träningen",
+    date: "2020-06-80"
+}]
+
+const app = express()
+app.use(express.json())
+//Make sure client folder is accesable
+app.use(express.static('./client'))
+
+// ### Get Todos  ### //
+app.get('/api/todos', (req,res) => res.json(todos))
+
+// ### Post Todos  ### //
+app.post('/api/todos', (req,res) => {
+    //console.log(req.body);
+    if(!req.body.title || !req.body.date){
+        res.status(404).json({
+            message:'Data sent as body is not correct'
+        })
+    }else{
+        const todo = {id: idIndex, ...req.body}
+        todos.push(todo)
+        res.status(201)
+        res.json(todo)
+        idIndex++
+    }
+
+})
+// ### Update Todos ### //
+app.put('/api/todos/:id', (req,res) => {
+    const paramId = req.params.id
+    let foundTodoIndex = todos.findIndex((todos) => todos.id == paramId)
+
+    if(foundTodoIndex == -1){
+        res.status(404).json({message:"Todos to update not found.."})
+    }
+    
+    todos[foundTodoIndex] = req.body
+    res.json({message: "Todos updated"})
+
+})
+
+// ### Remove Todos  ### //
+app.delete('/api/todos/:id', (req,res) => {
+    const paramId = req.params.id
+    let foundTodoIndex = todos.findIndex((todos) => todos.id == paramId)
+
+    if(foundTodoIndex == -1){
+        res.status(404).json({message: "Todos to delete not found.."})
+    }
+    todos.splice(foundTodoIndex, 1)
+
+    res.json({message: "Todos deleted"})
+})
+
+// ### Validation  ### //
+app.use((req,res) =>{
+    res.status(404).json({ 
+        message: 'Could not find the resource you are looking for....'
+    })
+})
+
+// Start the server
+app.listen(3000, () => console.log('Server is up and running on port 3000'))
